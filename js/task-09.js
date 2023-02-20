726187798964f2b55329d33d09cd8bd48982dfcc
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-02-20 18:45:45 +0200

    tasks 9, 10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  document.querySelector('span.color').textContent = body.style.backgroundColor;
 }
+
+const body = document.querySelector('body');
+
+const button = document.querySelector('button.change-color');
+
+button.addEventListener('click', getRandomHexColor);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input[type="number"]');
+
+const btnCreate = document.querySelector('button[data-create]');
+
+const btnDestroy = document.querySelector('button[data-destroy]');
+
+const boxes = document.querySelector('div#boxes');
+
+function createBoxes(amount) {
+  amount = input.value;
+  let divStr = '';
+  for (let i = 0; i < amount; i += 1) {
+    divStr += `<div style="background-color: ${getRandomHexColor()}; width: ${30 + i * 10}px; height: ${30 + i * 10}px;"></div>`;
+  };
+  boxes.insertAdjacentHTML('beforeend', divStr);
+};
+
+function destroyBoxes(event) {
+  boxes.innerHTML = '';
+};
+input.addEventListener('input', event => {});
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
