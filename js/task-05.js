c46f5539c1e645d5911cc947b9670ab752831eaf
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-02-21 16:24:21 +0200

    shorter ternar operator used in task 5

--- js/task-05.js
+++ js/task-05.js
@@ -4,8 +4,11 @@ const outputName = document.querySelector('#name-output');
 
 
 function getInputText(event) {
-    event.currentTarget.value === '' ?
-    outputName.textContent = 'Anonymous' :
-    outputName.textContent = event.currentTarget.value;
+    // event.currentTarget.value === '' ?
+    // outputName.textContent = 'Anonymous' :
+    // outputName.textContent = event.currentTarget.value;
+    outputName.textContent = event.currentTarget.value ?
+    event.currentTarget.value :
+    'Anonymous';
 }
 inputField.addEventListener('input', getInputText);
